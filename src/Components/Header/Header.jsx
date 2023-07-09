d4fae73a13929228621469801a86c42e3a01d3bf
priya-dharshini28 <139034705+priya-dharshini28@users.noreply.github.com>
2023-07-09 15:50:30 +0530

    Add files via upload

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Text,
+} from "@chakra-ui/react";
+
+export default function HeaderNav() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Center>
+          <Box p={6}>
+            <Text fontSize={"3xl"} fontWeight={"bold"}>
+              Video Gallery
+            </Text>
+          </Box>
+        </Center>
+      </Box>
+    </>
+  );
+}
