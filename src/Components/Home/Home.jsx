4d0ea0dcc1b6c95bdb53d6616e67bbfdbfed6f0f
priya-dharshini28 <139034705+priya-dharshini28@users.noreply.github.com>
2023-07-09 12:17:18 +0530

    Add files via upload

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderNav from "../Header/Header";
+import Video from "../Video/Video";
+
+function Home() {
+  return (
+    <div>
+      <HeaderNav />
+      <Video />
+    </div>
+  );
+}
+
+export default Home;
